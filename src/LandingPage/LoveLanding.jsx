08d1fef03e8d318160ab57da587ec033e00e1957
Vaibhav Mahendrasing Rajput <157829577+vaibhavrajput07@users.noreply.github.com>
2025-04-30 20:49:15 +0530

    Bolg option

--- src/LandingPage/LoveLanding.jsx
+++ src/LandingPage/LoveLanding.jsx
@@ -20,7 +20,7 @@ function LoveLanding() {
           <a href="#">Service</a>
           <a href="#">Gallery</a>
           <a href="#">Team</a>
-          <a href="#">Blog</a>
+          <a href="/showData">Blog</a>
           <a href="#">Contact</a>
         </nav>
 
